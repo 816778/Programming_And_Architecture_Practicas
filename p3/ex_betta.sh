2f62ce0314e23bf4ab3a55f9daef86bd6e5181c8
jaimebielsaeina <116299134+jaimebielsaeina@users.noreply.github.com>
2024-11-13 18:32:16 +0100

    p5 base
    CImg library added into p5 folder

---
+++ p3/ex_betta.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Asumiendo que el directorio actual es build-debug/Laboratory-3
+BUILD_FOLDER="build"
+RESULTS_FOLDER="results"
+
+# Compilar el programa
+make all
+
+output_file="$RESULTS_FOLDER/ex_β.txt"
+> $output_file
+
+echo "Running..."
+> $output_file
+
+steps=1048576
+thread=$(nproc)
+echo "Número de núcles = $thread"
+./$BUILD_FOLDER/pi_taylor_parallel_extra "$steps" "$thread" 
+
+echo "##################################################################"
+./$BUILD_FOLDER/pi_taylor_parallel_extra "$steps" 4
